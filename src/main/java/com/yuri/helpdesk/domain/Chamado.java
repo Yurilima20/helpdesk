212cd7154d4f9a20458af216de12803678d886e6
Yuri Gonçalves Lima <yurilima1231@gmail.com>
2025-07-27 11:08:13 -0300

    chore: Finalizing the addition pf dominant classes

--- src/main/java/com/yuri/helpdesk/domain/Chamado.java
+++ src/main/java/com/yuri/helpdesk/domain/Chamado.java
@@ -3,20 +3,41 @@ package com.yuri.helpdesk.domain;
 import java.time.LocalDate;
 import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.yuri.helpdesk.domain.enums.Prioridade;
 import com.yuri.helpdesk.domain.enums.Status;
 
+@Entity
 public class Chamado {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	
+	@JsonFormat( pattern = "dd/MM/yyyy")
 	private LocalDate dataAbertura = LocalDate.now();
+	
+	@JsonFormat( pattern = "dd/MM/yyyy")
 	private LocalDate dataFechamento; 
+	
 	private Prioridade prioridade;
 	private Status status; 
 	private String titulo; 
 	private String observacoes;
 	
+	@ManyToOne
+	@JoinColumn(name = "tecnico_id")
 	private Tecnico tecnico;
+	
+	@ManyToOne
+	@JoinColumn(name = "cliente_id")
 	private Cliente cliente;
 	
 	public Chamado() {
--- src/main/java/com/yuri/helpdesk/domain/Tecnico.java
+++ src/main/java/com/yuri/helpdesk/domain/Tecnico.java
@@ -3,8 +3,14 @@ package com.yuri.helpdesk.domain;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+
+@Entity
 public class Tecnico extends Pessoa {
+	private static final long serialVersionUID = 1L;
 	
+	@OneToMany(mappedBy = "cliente")
 	private List<Chamado> chamados = new ArrayList<>();
 
 	public Tecnico() {
