cf23da12a250f180a0330fcf3574c6a1545df95d
Yuri Gonçalves Lima <yurilima1231@gmail.com>
2025-08-03 14:14:24 -0300

    exceptions for CPF and EMAIL created with sucess

--- src/main/java/com/yuri/helpdesk/repositories/PessoaRepository.java
+++ src/main/java/com/yuri/helpdesk/repositories/PessoaRepository.java
@@ -1,9 +1,13 @@
 package com.yuri.helpdesk.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.yuri.helpdesk.domain.Pessoa;
 
 public interface PessoaRepository extends JpaRepository<Pessoa, Integer>{
 
+	Optional<Pessoa> findByCpf(String cpf);
+	Optional<Pessoa> findByEmail(String email);
 }
