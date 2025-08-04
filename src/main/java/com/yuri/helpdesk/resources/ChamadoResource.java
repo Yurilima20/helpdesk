7a188b7aa3ff8912daffb99a4c404fc25d7304a3
Yuri Gonçalves Lima <yurilima1231@gmail.com>
2025-08-04 11:22:34 -0300

    feat: implements findAll for chamados

--- src/main/java/com/yuri/helpdesk/resources/ChamadoResource.java
+++ src/main/java/com/yuri/helpdesk/resources/ChamadoResource.java
@@ -1,5 +1,10 @@
 package com.yuri.helpdesk.resources;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.swing.text.html.parser.Entity;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,4 +29,11 @@ public class ChamadoResource {
 		return ResponseEntity.ok().body(new ChamadoDTO(obj));
 	}
 	
+	@GetMapping
+	public ResponseEntity<List<ChamadoDTO>> findAll(){
+		List<Chamado> list = service.findAll();
+		List<ChamadoDTO> listDTO = list.stream().map(obj -> new ChamadoDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
+	}
+ 	
 }
--- src/main/java/com/yuri/helpdesk/services/ChamadoService.java
+++ src/main/java/com/yuri/helpdesk/services/ChamadoService.java
@@ -1,5 +1,6 @@
 package com.yuri.helpdesk.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,13 +12,17 @@ import com.yuri.helpdesk.services.exceptions.ObjectnotFoundException;
 
 @Service
 public class ChamadoService {
-	
+
 	@Autowired
 	private ChamadoRepository repository;
-	
+
 	public Chamado findById(Integer id) {
 		Optional<Chamado> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectnotFoundException("Objeto não encontrado! ID:" + id));
+	}
+
+	public List<Chamado> findAll() {
+		return repository.findAll();
 	}
 
 }
